3757752a3bceae245d3205567437a8e068f99479
tinghsuanhsu <tinghsuanhsu@gmail.com>
2026-04-07 10:36:55 +1000

    Timestamp update

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const securityHeaders = [
   { key: 'X-Content-Type-Options', value: 'nosniff' },
   { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-  { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=()' },
+  { key: 'Permissions-Policy', value: 'camera=(self), microphone=(), geolocation=()' },
   {
     key: 'Content-Security-Policy',
     value:
